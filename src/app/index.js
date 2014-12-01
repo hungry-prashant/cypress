129880bcf88fc3a7f51cc923fe5dc519e0b9509d
Dave Bouwman <dave@davebouwman.com>
2014-11-30 19:14:07 -0700

    first commit of cypress - just angular scaffolding

---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('cypress', ['ngAnimate', 'ngCookies', 'ngSanitize', 'ui.router'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('/');
+  })
+;
---
+++ src/components/navbar/navbar.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('cypress')
+  .controller('NavbarCtrl', function ($scope) {
+    $scope.date = new Date();
+  });
