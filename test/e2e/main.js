71d09518caf2462c5aa8cb3303755b47d995826d
hungry_prashant <bhawsar.prasanth@gmail.com>
2020-10-09 11:29:29 +0530

    null check for element to be tested
    hacktoberfest

--- test/e2e/main.js
+++ test/e2e/main.js
@@ -7,9 +7,11 @@ describe('The main view', function () {
   });
 
   it('list more than 5 awesome things', function () {
-    element.all(by.repeater('awesomeThing in awesomeThings')).count().then(function(count) {
-      expect(count > 5).toBeTruthy();
-    });
+    if(element){ // first check if element is available or not
+      element.all(by.repeater('awesomeThing in awesomeThings')).count().then(function(count) {
+        expect(count > 5).toBeTruthy();
+      });
+    }
   });
 
 });
